bc7a07b8ad3be0d18170b968f4d2d00ebef702f4
Dmytro Fedoriaka <fedimser@gmail.com>
2026-09-08 21:21:24 -0700

    switch to pyright

--- lint.sh
+++ lint.sh
@@ -4,12 +4,8 @@ echo "Running black..."
 black --check ./aldyparen
 result+=$?
 
-echo "Running pylint..."
-pylint ./aldyparen
-result+=$?
-
-echo "Running mypy..."
-mypy ./aldyparen
+echo "Running pyright..."
+pyright ./aldyparen
 result+=$?
 
 exit $result
